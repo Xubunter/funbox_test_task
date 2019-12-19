0e94653181237f7928eea8ac7834211939f4a0d8
Xubunter <xubunter@gmail.com>
2019-12-19 14:27:12 +0300

    dist for gh-pages

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import './style.scss';
-import './fonts/fonts.scss'
-
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App/>, mountNode);
